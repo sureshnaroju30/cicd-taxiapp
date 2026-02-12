04fdc118292e77c458097ba4d8dcf8ef00895fb7
suresh <sbnaroju@gmail.com>
2026-02-12 17:07:20 +0530

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-#!/bin/bash 
-kubectl apply -f k8s/deployment.yaml 
-kubectl apply -f k8s/service.yaml 
-kubectl apply -f k8s/secrets.yaml 
+#!/bin/bash
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
+kubectl apply -f k8s/secrets.yaml
 kubectl apply -f k8s/namespace.yaml 
